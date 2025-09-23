4e1a261a32bb5472019959b992b3b87458ce2ab2
Zhiheng Chen <ericczh1@gmail.com>
2025-09-23 13:03:46 -0400

    Add VDP and LV systems

--- SINDyFunctions.py
+++ SINDyFunctions.py
@@ -80,6 +80,72 @@ def simulateRosslerSystem(X0,Deltat_sim,gridDensity,a,b,c):
     X_dot_out = np.hstack([x_dot,y_dot,z_dot])
     return [t_out,X_out,X_dot_out]
 
+# ---van der Pol oscillator simulations---
+def VDPEqs(t,X,mu):
+    x = X[0]
+    y = X[1]
+    x_dot = y
+    y_dot = -mu*x**2*y-x+mu*y
+    return [x_dot,y_dot]
+
+def simulateVDP(X0,Deltat_sim,gridDensity,mu):
+    # numerical integration
+    t_span = [0,Deltat_sim]
+    t_eval = np.linspace(0,Deltat_sim,gridDensity*Deltat_sim+1)
+    soln = scipy.integrate.solve_ivp(VDPEqs,t_span,X0,args=[mu],method="RK45",t_eval=t_eval)
+    t = soln.t
+    [x,y] = soln.y
+    # exact derivatives from simulation
+    x_dot = []
+    y_dot = []
+    for i in range(0,len(t)):
+        X = [x[i],y[i]]
+        [dxdt,dydt] = VDPEqs(t,X,mu)
+        x_dot.append(dxdt)
+        y_dot.append(dydt)
+    # log data
+    t_out = np.array(t).reshape(len(t),1)
+    x = np.array(x).reshape(len(t),1)
+    y = np.array(y).reshape(len(t),1)
+    X_out = np.hstack([x,y])
+    x_dot = np.array(x_dot).reshape(len(t),1)
+    y_dot = np.array(y_dot).reshape(len(t),1)
+    X_dot_out = np.hstack([x_dot,y_dot])
+    return [t_out,X_out,X_dot_out]
+
+# ---Lotka-Volterra system simulations---
+def LVEqs(t,X,beta):
+    x = X[0]
+    y = X[1]
+    x_dot = 3*x-beta*x*y
+    y_dot = -6*y+beta*x*y
+    return [x_dot,y_dot]
+
+def simulateLV(X0,Deltat_sim,gridDensity,beta):
+    # numerical integration
+    t_span = [0,Deltat_sim]
+    t_eval = np.linspace(0,Deltat_sim,gridDensity*Deltat_sim+1)
+    soln = scipy.integrate.solve_ivp(LVEqs,t_span,X0,args=[beta],method="RK45",t_eval=t_eval)
+    t = soln.t
+    [x,y] = soln.y
+    # exact derivatives from simulation
+    x_dot = []
+    y_dot = []
+    for i in range(0,len(t)):
+        X = [x[i],y[i]]
+        [dxdt,dydt] = LVEqs(t,X,beta)
+        x_dot.append(dxdt)
+        y_dot.append(dydt)
+    # log data
+    t_out = np.array(t).reshape(len(t),1)
+    x = np.array(x).reshape(len(t),1)
+    y = np.array(y).reshape(len(t),1)
+    X_out = np.hstack([x,y])
+    x_dot = np.array(x_dot).reshape(len(t),1)
+    y_dot = np.array(y_dot).reshape(len(t),1)
+    X_dot_out = np.hstack([x_dot,y_dot])
+    return [t_out,X_out,X_dot_out]
+
 # ---library---
 def calcTheta_Lorenz(arr_t,arr_x,arr_y,arr_z):
     Theta = np.zeros([len(arr_t),10])
@@ -97,6 +163,36 @@ def calcTheta_Lorenz(arr_t,arr_x,arr_y,arr_z):
         Theta[i,7] = x*y
         Theta[i,8] = y*z
         Theta[i,9] = x*z
+    return Theta
+
+def calcTheta_VDP(arr_t,arr_x,arr_y):
+    Theta = np.zeros([len(arr_t),10])
+    for i in range(0,len(arr_t)):
+        x = arr_x[i]
+        y = arr_y[i]
+        Theta[i,0] = 1
+        Theta[i,1] = x
+        Theta[i,2] = y
+        Theta[i,3] = x**2
+        Theta[i,4] = y**2
+        Theta[i,5] = x*y
+        Theta[i,6] = x**3
+        Theta[i,7] = y**3
+        Theta[i,8] = x**2*y
+        Theta[i,9] = x*y**2
+    return Theta
+
+def calcTheta_LV(arr_t,arr_x,arr_y):
+    Theta = np.zeros([len(arr_t),6])
+    for i in range(0,len(arr_t)):
+        x = arr_x[i]
+        y = arr_y[i]
+        Theta[i,0] = 1
+        Theta[i,1] = x
+        Theta[i,2] = y
+        Theta[i,3] = x**2
+        Theta[i,4] = y**2
+        Theta[i,5] = x*y
     return Theta
 
 # ---sparse regression---
@@ -186,6 +282,27 @@ def calcDerivatives(t_out,X_out):
     z_dot = z_dot.reshape(len(z_dot),1)
     return x_dot,y_dot,z_dot
 
+def calcDerivatives_2D(t_out,X_out):
+    Deltat = np.diff(t_out.reshape(1,len(t_out)))
+    Deltax = np.diff(X_out[:,0].reshape(1,len(t_out)))
+    Deltay = np.diff(X_out[:,1].reshape(1,len(t_out)))
+    t_out = t_out[1:-1]
+    X_out = X_out[1:-1,:]
+    # forward step size
+    a = Deltat[0,1:]
+    Deltax_fwd = Deltax[0,1:]
+    Deltay_fwd = Deltay[0,1:]
+    # backward step size
+    b = Deltat[0,0:-1]
+    Deltax_bwd = Deltax[0,0:-1]
+    Deltay_bwd = Deltay[0,0:-1]
+    # derivatives
+    x_dot = (b**2*Deltax_fwd+a**2*Deltax_bwd)/(a**2*b+a*b**2)
+    y_dot = (b**2*Deltay_fwd+a**2*Deltay_bwd)/(a**2*b+a*b**2)
+    x_dot = x_dot.reshape(len(x_dot),1)
+    y_dot = y_dot.reshape(len(y_dot),1)
+    return x_dot,y_dot
+
 # SINDy
 def SINDy_Lorenz(t_out,X_out,params_regression):
     x_dot,y_dot,z_dot = calcDerivatives(t_out,X_out)
@@ -200,6 +317,32 @@ def SINDy_Lorenz(t_out,X_out,params_regression):
         w3 = sparseRegression_prescribedLambda(Theta,z_dot,params_regression)
 
     w = np.hstack([w1,w2,w3])
+    return w
+
+def SINDy_VDP(t_out,X_out,params_regression):
+    x_dot,y_dot = calcDerivatives_2D(t_out,X_out)
+    Theta = calcTheta_VDP(t_out[1:-1],X_out[1:-1,0],X_out[1:-1,1])
+    if params_regression["lambda_sparse"] == "auto":
+        w1 = sparseRegression_autoLambda(Theta,x_dot,params_regression)
+        w2 = sparseRegression_autoLambda(Theta,y_dot,params_regression)
+    else:
+        w1 = sparseRegression_prescribedLambda(Theta,x_dot,params_regression)
+        w2 = sparseRegression_prescribedLambda(Theta,y_dot,params_regression)
+
+    w = np.hstack([w1,w2])
+    return w
+
+def SINDy_LV(t_out,X_out,params_regression):
+    x_dot,y_dot = calcDerivatives_2D(t_out,X_out)
+    Theta = calcTheta_LV(t_out[1:-1],X_out[1:-1,0],X_out[1:-1,1])
+    if params_regression["lambda_sparse"] == "auto":
+        w1 = sparseRegression_autoLambda(Theta,x_dot,params_regression)
+        w2 = sparseRegression_autoLambda(Theta,y_dot,params_regression)
+    else:
+        w1 = sparseRegression_prescribedLambda(Theta,x_dot,params_regression)
+        w2 = sparseRegression_prescribedLambda(Theta,y_dot,params_regression)
+
+    w = np.hstack([w1,w2])
     return w
 
 # ---weak SINDy---
@@ -275,6 +418,120 @@ def WSINDy_bump_Lorenz(t_out,X_out,N_p,N_q,params_regression):
         w3 = sparseRegression_prescribedLambda(A_trapz,b3_trapz,params_regression)
 
     w = np.hstack([w1,w2,w3])
+    return w
+
+def WSINDy_bump_VDP(t_out,X_out,N_p,N_q,params_regression):
+    # library
+    Theta = calcTheta_VDP(t_out,X_out[:,0],X_out[:,1])
+    # test function parameters
+    array_p = np.linspace(1,100,N_p)
+    array_q = np.linspace(1,100,N_q)
+    # find V and V_dot for Riemann sums
+    Deltat = np.diff(t_out.reshape(1,len(t_out)))
+    V_L = np.zeros([N_p*N_q,len(t_out)-1])
+    V_R = np.zeros([N_p*N_q,len(t_out)-1])
+    V_dot_L = np.zeros([N_p*N_q,len(t_out)-1])
+    V_dot_R = np.zeros([N_p*N_q,len(t_out)-1])
+    for n_p in range(0,N_p):
+        for n_q in range(0,N_q):
+            p = array_p[n_p]
+            q = array_q[n_q]
+            m = n_p*N_q+n_q   #(m+1)-th test function with index m
+            # V_L for left Riemann sum
+            Phi_L_m = func_phi_bump(t_out[0:-1],p,q,t_out[0],t_out[-1])
+            Phi_L_m = Phi_L_m.reshape(1,len(Phi_L_m))
+            V_L[m,:] = Deltat*Phi_L_m
+            # V_R for right Riemann sum
+            Phi_R_m = func_phi_bump(t_out[1:],p,q,t_out[0],t_out[-1])
+            Phi_R_m = Phi_R_m.reshape(1,len(Phi_R_m))
+            V_R[m,:] = Deltat*Phi_R_m
+            # V_dot_L for left Riemann sum
+            Phi_dot_L_m = func_phi_dot_bump(t_out[0:-1],p,q,t_out[0],t_out[-1])
+            Phi_dot_L_m = Phi_dot_L_m.reshape(1,len(Phi_dot_L_m))
+            V_dot_L[m,:] = Deltat*Phi_dot_L_m
+            # V_dot_R for right Riemann sum
+            Phi_dot_R_m = func_phi_dot_bump(t_out[1:],p,q,t_out[0],t_out[-1])
+            Phi_dot_R_m = Phi_dot_R_m.reshape(1,len(Phi_dot_R_m))
+            V_dot_R[m,:] = Deltat*Phi_dot_R_m
+    # compute A and b from trapezoid integral
+    ## A
+    A_L = V_L@Theta[0:-1,:]
+    A_R = V_R@Theta[1:,:]
+    A_trapz = (A_L+A_R)/2
+    ## b1
+    b1_L = -V_dot_L@X_out[0:-1,0]
+    b1_R = -V_dot_R@X_out[1:,0]
+    b1_trapz = (b1_L+b1_R)/2
+    ## b2
+    b2_L = -V_dot_L@X_out[0:-1,1]
+    b2_R = -V_dot_R@X_out[1:,1]
+    b2_trapz = (b2_L+b2_R)/2
+    # sparse regression
+    if params_regression["lambda_sparse"] == "auto":
+        w1 = sparseRegression_autoLambda(A_trapz,b1_trapz,params_regression)
+        w2 = sparseRegression_autoLambda(A_trapz,b2_trapz,params_regression)   
+    else:
+        w1 = sparseRegression_prescribedLambda(A_trapz,b1_trapz,params_regression)
+        w2 = sparseRegression_prescribedLambda(A_trapz,b2_trapz,params_regression)   
+
+    w = np.hstack([w1,w2])
+    return w
+
+def WSINDy_bump_LV(t_out,X_out,N_p,N_q,params_regression):
+    # library
+    Theta = calcTheta_LV(t_out,X_out[:,0],X_out[:,1])
+    # test function parameters
+    array_p = np.linspace(1,100,N_p)
+    array_q = np.linspace(1,100,N_q)
+    # find V and V_dot for Riemann sums
+    Deltat = np.diff(t_out.reshape(1,len(t_out)))
+    V_L = np.zeros([N_p*N_q,len(t_out)-1])
+    V_R = np.zeros([N_p*N_q,len(t_out)-1])
+    V_dot_L = np.zeros([N_p*N_q,len(t_out)-1])
+    V_dot_R = np.zeros([N_p*N_q,len(t_out)-1])
+    for n_p in range(0,N_p):
+        for n_q in range(0,N_q):
+            p = array_p[n_p]
+            q = array_q[n_q]
+            m = n_p*N_q+n_q   #(m+1)-th test function with index m
+            # V_L for left Riemann sum
+            Phi_L_m = func_phi_bump(t_out[0:-1],p,q,t_out[0],t_out[-1])
+            Phi_L_m = Phi_L_m.reshape(1,len(Phi_L_m))
+            V_L[m,:] = Deltat*Phi_L_m
+            # V_R for right Riemann sum
+            Phi_R_m = func_phi_bump(t_out[1:],p,q,t_out[0],t_out[-1])
+            Phi_R_m = Phi_R_m.reshape(1,len(Phi_R_m))
+            V_R[m,:] = Deltat*Phi_R_m
+            # V_dot_L for left Riemann sum
+            Phi_dot_L_m = func_phi_dot_bump(t_out[0:-1],p,q,t_out[0],t_out[-1])
+            Phi_dot_L_m = Phi_dot_L_m.reshape(1,len(Phi_dot_L_m))
+            V_dot_L[m,:] = Deltat*Phi_dot_L_m
+            # V_dot_R for right Riemann sum
+            Phi_dot_R_m = func_phi_dot_bump(t_out[1:],p,q,t_out[0],t_out[-1])
+            Phi_dot_R_m = Phi_dot_R_m.reshape(1,len(Phi_dot_R_m))
+            V_dot_R[m,:] = Deltat*Phi_dot_R_m
+    # compute A and b from trapezoid integral
+    ## A
+    A_L = V_L@Theta[0:-1,:]
+    A_R = V_R@Theta[1:,:]
+    A_trapz = (A_L+A_R)/2
+    ## b1
+    b1_L = -V_dot_L@X_out[0:-1,0]
+    b1_R = -V_dot_R@X_out[1:,0]
+    b1_trapz = (b1_L+b1_R)/2
+    ## b2
+    b2_L = -V_dot_L@X_out[0:-1,1]
+    b2_R = -V_dot_R@X_out[1:,1]
+    b2_trapz = (b2_L+b2_R)/2
+    # sparse regression
+    if params_regression["lambda_sparse"] == "auto":
+        w1 = sparseRegression_autoLambda(A_trapz,b1_trapz,params_regression)
+        w2 = sparseRegression_autoLambda(A_trapz,b2_trapz,params_regression)   
+    else:
+        w1 = sparseRegression_prescribedLambda(A_trapz,b1_trapz,params_regression)
+        w2 = sparseRegression_prescribedLambda(A_trapz,b2_trapz,params_regression)   
+
+    w = np.hstack([w1,w2])
     return w
 
 # Fourier test functions
@@ -418,6 +675,52 @@ def WSINDy_Fourier_FFT_Lorenz(t_out,X_out,N_freq,params_regression):
     w = np.hstack([w1,w2,w3])
     return w
 
+def WSINDy_Fourier_FFT_VDP(t_out,X_out,N_freq,params_regression):
+    omega0 = 2*np.pi/(t_out[-1]-t_out[0])
+    arr_x = X_out[:,0]
+    arr_y = X_out[:,1]
+    # set n-values
+    arr_n_x = np.linspace(1,N_freq,N_freq)
+    arr_n_y = np.linspace(1,N_freq,N_freq)
+    # library
+    Theta = calcTheta_VDP(t_out,arr_x,arr_y)
+    # A and b
+    A1,b1 = constructLS_Fourier_FFT(t_out,arr_x,arr_n_x,Theta)
+    A2,b2 = constructLS_Fourier_FFT(t_out,arr_y,arr_n_y,Theta)
+    # sparse regressions
+    if params_regression["lambda_sparse"] == "auto":
+        w1 = sparseRegression_autoLambda(A1,b1,params_regression)
+        w2 = sparseRegression_autoLambda(A2,b2,params_regression)
+    else:
+        w1 = sparseRegression_prescribedLambda(A1,b1,params_regression)
+        w2 = sparseRegression_prescribedLambda(A2,b2,params_regression)
+
+    w = np.hstack([w1,w2])
+    return w
+
+def WSINDy_Fourier_FFT_LV(t_out,X_out,N_freq,params_regression):
+    omega0 = 2*np.pi/(t_out[-1]-t_out[0])
+    arr_x = X_out[:,0]
+    arr_y = X_out[:,1]
+    # set n-values
+    arr_n_x = np.linspace(1,N_freq,N_freq)
+    arr_n_y = np.linspace(1,N_freq,N_freq)
+    # library
+    Theta = calcTheta_LV(t_out,arr_x,arr_y)
+    # A and b
+    A1,b1 = constructLS_Fourier_FFT(t_out,arr_x,arr_n_x,Theta)
+    A2,b2 = constructLS_Fourier_FFT(t_out,arr_y,arr_n_y,Theta)
+    # sparse regressions
+    if params_regression["lambda_sparse"] == "auto":
+        w1 = sparseRegression_autoLambda(A1,b1,params_regression)
+        w2 = sparseRegression_autoLambda(A2,b2,params_regression)
+    else:
+        w1 = sparseRegression_prescribedLambda(A1,b1,params_regression)
+        w2 = sparseRegression_prescribedLambda(A2,b2,params_regression)
+
+    w = np.hstack([w1,w2])
+    return w
+
 # ---error evaluations---
 def errorEval(w_true,w_ident):
     # relative error norm
@@ -431,3 +734,37 @@ def errorEval(w_true,w_ident):
     N_correct = np.sum((w_true==0) & (w_ident==0))
     TPR = N_correct/(N_correct+N_spurious+N_failed)   
     return errorNorm_rel,TPR
+
+def batchErrorEval(w_true, arr_w):
+    n_noiseLevels = arr_w.shape[2]
+    N_noise = arr_w.shape[3]
+
+    error_all = np.zeros((n_noiseLevels, N_noise))
+    TPR_all = np.zeros((n_noiseLevels, N_noise))
+
+    # loop over noise levels and trials
+    for i in range(n_noiseLevels):
+        for j in range(N_noise):
+            w_ident = arr_w[:,:,i,j]
+            eNorm, TPR = errorEval(w_true, w_ident)
+            error_all[i,j] = eNorm
+            TPR_all[i,j] = TPR
+
+    # compute statistics
+    error_mean = np.mean(error_all,axis=1)
+    error_q1 = np.percentile(error_all,25,axis=1)
+    error_q3 = np.percentile(error_all,75,axis=1)
+    TPR_mean = np.mean(TPR_all,axis=1)
+    TPR_q1 = np.percentile(TPR_all,25,axis=1)
+    TPR_q3 = np.percentile(TPR_all,75,axis=1)
+
+    results = {
+        "error_mean": error_mean,
+        "error_q1": error_q1,
+        "error_q3": error_q3,
+        "TPR_mean": TPR_mean,
+        "TPR_q1": TPR_q1,
+        "TPR_q3": TPR_q3
+    }
+
+    return results
--- errorEvaluations_Lorenz.py
+++ errorEvaluations_Lorenz.py
@@ -49,31 +49,15 @@ params_regression["lambda_ridge"] = 0.001
 params_regression["N_loops"] = 100
 
 # evaluate error at different noise levels
-arr_relError_mean = np.zeros((1,len(arr_sig_NR))) # allocate array of mean and standard dev. of relative error norms for each noise level
-arr_relError_std = np.zeros((1,len(arr_sig_NR)))
-arr_TPR_mean = np.zeros((1,len(arr_sig_NR))) # allocate array of mean and standard dev. of TPR for each noise level
-arr_TPR_std = np.zeros((1,len(arr_sig_NR)))
+arr_w_SINDy = np.zeros((w_true.shape[0],w_true.shape[1],len(arr_sig_NR),N_noise))
 
 startTime = time.perf_counter()
 for i in range(0,len(arr_sig_NR)):
-    arr_relError = np.zeros((1,N_noise))   # array of relative error norms for N noises with current noise level
-    arr_TPR = np.zeros((1,N_noise)) # array of TPRs for N noises with current noise level
     for j in range(0,N_noise):
         X_noisy = arr_X_noisy[:,:,i,j]
         w_ident = SINDy_Lorenz(t_out,X_noisy,params_regression)
-        errorNorm_rel,TPR = errorEval(w_true,w_ident)
-        arr_relError[0,j] = errorNorm_rel
-        arr_TPR[0,j] = TPR
+        arr_w_SINDy[:,:,i,j] = w_ident
         print(f"Progress 1/4: {i*N_noise+(j+1)}/{len(arr_sig_NR)*N_noise}")
-    arr_relError_mean[0,i] = np.average(arr_relError)
-    arr_relError_std[0,i] = np.std(arr_relError)
-    arr_TPR_mean[0,i] = np.average(arr_TPR)
-    arr_TPR_std[0,i] = np.std(arr_TPR)
-errorMean_SINDy_Lorenz = arr_relError_mean.flatten()
-errorStD_SINDy_Lorenz = arr_relError_std
-TPRMean_SINDy_Lorenz = arr_TPR_mean.flatten()
-TPRStD_SINDy_Lorenz = arr_TPR_std.flatten()
-
 endTime = time.perf_counter()
 time_SINDy = endTime-startTime
 print("!")
@@ -87,31 +71,15 @@ params_regression["lambda_ridge"] = 0.001
 params_regression["N_loops"] = 100
 
 # evaluate error at different noise levels
-arr_relError_mean = np.zeros((1,len(arr_sig_NR))) # allocate array of mean and standard dev. of relative error norms for each noise level
-arr_relError_std = np.zeros((1,len(arr_sig_NR)))
-arr_TPR_mean = np.zeros((1,len(arr_sig_NR))) # allocate array of mean and standard dev. of TPR for each noise level
-arr_TPR_std = np.zeros((1,len(arr_sig_NR)))
+arr_w_bumpWSINDy = np.zeros((w_true.shape[0],w_true.shape[1],len(arr_sig_NR),N_noise))
 
 startTime = time.perf_counter()
 for i in range(0,len(arr_sig_NR)):
-    arr_relError = np.zeros((1,N_noise))   # array of relative error norms for N noises with current noise level
-    arr_TPR = np.zeros((1,N_noise)) # array of TPRs for N noises with current noise level
     for j in range(0,N_noise):
         X_noisy = arr_X_noisy[:,:,i,j]
         w_ident = WSINDy_bump_Lorenz(t_out,X_noisy,20,20,params_regression)
-        errorNorm_rel,TPR = errorEval(w_true,w_ident)
-        arr_relError[0,j] = errorNorm_rel
-        arr_TPR[0,j] = TPR
+        arr_w_bumpWSINDy[:,:,i,j] = w_ident
         print(f"Progress 2/4: {i*N_noise+(j+1)}/{len(arr_sig_NR)*N_noise}")
-    arr_relError_mean[0,i] = np.average(arr_relError)
-    arr_relError_std[0,i] = np.std(arr_relError)
-    arr_TPR_mean[0,i] = np.average(arr_TPR)
-    arr_TPR_std[0,i] = np.std(arr_TPR)
-errorMean_bumpWSINDy_Lorenz = arr_relError_mean.flatten()
-errorStD_bumpWSINDy_Lorenz = arr_relError_std
-TPRMean_bumpWSINDy_Lorenz = arr_TPR_mean.flatten()
-TPRStD_bumpWSINDy_Lorenz = arr_TPR_std.flatten()
-
 endTime = time.perf_counter()
 time_bumpWSINDy = endTime-startTime
 print("!")
@@ -126,32 +94,15 @@ params_regression["N_loops"] = 100
 
 # evaluate error at different noise levels
 N_freq = 60
-
-arr_relError_mean = np.zeros((1,len(arr_sig_NR))) # allocate array of mean and standard dev. of relative error norms for each noise level
-arr_relError_std = np.zeros((1,len(arr_sig_NR)))
-arr_TPR_mean = np.zeros((1,len(arr_sig_NR))) # allocate array of mean and standard dev. of TPR for each noise level
-arr_TPR_std = np.zeros((1,len(arr_sig_NR)))
+arr_w_FourierWSINDy = np.zeros((w_true.shape[0],w_true.shape[1],len(arr_sig_NR),N_noise))
 
 startTime = time.perf_counter()
 for i in range(0,len(arr_sig_NR)):
-    arr_relError = np.zeros((1,N_noise))   # array of relative error norms for N noises with current noise level
-    arr_TPR = np.zeros((1,N_noise)) # array of TPRs for N noises with current noise level
     for j in range(0,N_noise):
         X_noisy = arr_X_noisy[:,:,i,j]
         w_ident = WSINDy_Fourier_Lorenz(t_out,X_noisy,N_freq,params_regression)
-        errorNorm_rel,TPR = errorEval(w_true,w_ident)
-        arr_relError[0,j] = errorNorm_rel
-        arr_TPR[0,j] = TPR
+        arr_w_FourierWSINDy[:,:,i,j] = w_ident
         print(f"Progress 3/4: {i*N_noise+(j+1)}/{len(arr_sig_NR)*N_noise}")
-    arr_relError_mean[0,i] = np.average(arr_relError)
-    arr_relError_std[0,i] = np.std(arr_relError)
-    arr_TPR_mean[0,i] = np.average(arr_TPR)
-    arr_TPR_std[0,i] = np.std(arr_TPR)
-errorMean_FourierWSINDy_Lorenz = arr_relError_mean.flatten()
-errorStD_FourierWSINDy_Lorenz = arr_relError_std
-TPRMean_FourierWSINDy_Lorenz = arr_TPR_mean.flatten()
-TPRStD_FourierWSINDy_Lorenz = arr_TPR_std.flatten()
-
 endTime = time.perf_counter()
 time_FourierWSINDy = endTime-startTime
 print("!")
@@ -166,32 +117,15 @@ params_regression["N_loops"] = 100
 
 # evaluate error at different noise levels
 N_freq = 60
-
-arr_relError_mean = np.zeros((1,len(arr_sig_NR))) # allocate array of mean and standard dev. of relative error norms for each noise level
-arr_relError_std = np.zeros((1,len(arr_sig_NR)))
-arr_TPR_mean = np.zeros((1,len(arr_sig_NR))) # allocate array of mean and standard dev. of TPR for each noise level
-arr_TPR_std = np.zeros((1,len(arr_sig_NR)))
+arr_w_FFTWSINDy = np.zeros((w_true.shape[0],w_true.shape[1],len(arr_sig_NR),N_noise))
 
 startTime = time.perf_counter()
 for i in range(0,len(arr_sig_NR)):
-    arr_relError = np.zeros((1,N_noise))   # array of relative error norms for N noises with current noise level
-    arr_TPR = np.zeros((1,N_noise)) # array of TPRs for N noises with current noise level
     for j in range(0,N_noise):
         X_noisy = arr_X_noisy[:,:,i,j]
         w_ident = WSINDy_Fourier_FFT_Lorenz(t_out,X_noisy,N_freq,params_regression)
-        errorNorm_rel,TPR = errorEval(w_true,w_ident)
-        arr_relError[0,j] = errorNorm_rel
-        arr_TPR[0,j] = TPR
+        arr_w_FFTWSINDy[:,:,i,j] = w_ident
         print(f"Progress 4/4: {i*N_noise+(j+1)}/{len(arr_sig_NR)*N_noise}")
-    arr_relError_mean[0,i] = np.average(arr_relError)
-    arr_relError_std[0,i] = np.std(arr_relError)
-    arr_TPR_mean[0,i] = np.average(arr_TPR)
-    arr_TPR_std[0,i] = np.std(arr_TPR)
-errorMean_FourierWSINDyFFT_Lorenz = arr_relError_mean.flatten()
-errorStD_FourierWSINDyFFT_Lorenz = arr_relError_std
-TPRMean_FourierWSINDyFFT_Lorenz = arr_TPR_mean.flatten()
-TPRStD_FourierWSINDyFFT_Lorenz = arr_TPR_std.flatten()
-
 endTime = time.perf_counter()
 time_FourierWSINDyFFT = endTime-startTime
 print("!")
@@ -199,25 +133,13 @@ print("!")
 # ---log data---
 data = dict()
 data["arr_sig_NR"] = arr_sig_NR
-data["errorMean_SINDy_Lorenz"] = errorMean_SINDy_Lorenz
-data["errorStD_SINDy_Lorenz"] = errorStD_SINDy_Lorenz
-data["TPRMean_SINDy_Lorenz"] = TPRMean_SINDy_Lorenz
-data["TPRStD_SINDy_Lorenz"] = TPRStD_SINDy_Lorenz
-data["errorMean_bumpWSINDy_Lorenz"] = errorMean_bumpWSINDy_Lorenz
-data["errorStD_bumpWSINDy_Lorenz"] = errorStD_bumpWSINDy_Lorenz
-data["TPRMean_bumpWSINDy_Lorenz"] = TPRMean_bumpWSINDy_Lorenz
-data["TPRStD_bumpWSINDy_Lorenz"] = TPRStD_bumpWSINDy_Lorenz
-data["errorMean_FourierWSINDy_Lorenz"] = errorMean_FourierWSINDy_Lorenz
-data["errorStD_FourierWSINDy_Lorenz"] = errorStD_FourierWSINDy_Lorenz
-data["TPRMean_FourierWSINDy_Lorenz"] = TPRMean_FourierWSINDy_Lorenz
-data["TPRStD_FourierWSINDy_Lorenz"] = TPRStD_FourierWSINDy_Lorenz
-data["errorMean_FourierWSINDyFFT_Lorenz"] = errorMean_FourierWSINDyFFT_Lorenz
-data["errorStD_FourierWSINDyFFT_Lorenz"] = errorStD_FourierWSINDyFFT_Lorenz
-data["TPRMean_FourierWSINDyFFT_Lorenz"] = TPRMean_FourierWSINDyFFT_Lorenz
-data["TPRStD_FourierWSINDyFFT_Lorenz"] = TPRStD_FourierWSINDyFFT_Lorenz
+data["arr_w_SINDy"] = arr_w_SINDy
+data["arr_w_bumpWSINDy"] = arr_w_bumpWSINDy
+data["arr_w_FourierWSINDy"] = arr_w_FourierWSINDy
+data["arr_w_FFTWSINDy"] = arr_w_FFTWSINDy
 data["time_SINDy"] = time_SINDy
 data["time_bumpWSINDy"] = time_bumpWSINDy
 data["time_FourierWSINDy"] = time_FourierWSINDy
-data["time_FourierWSINDyFFT"] = time_FourierWSINDyFFT
+data["time_FFTWSINDy"] = time_FourierWSINDyFFT
 
-savemat("results_noiseLevel_Lorenz.mat",data)
+savemat("results_noiseLevelAndCoeffs_Lorenz.mat",data)
